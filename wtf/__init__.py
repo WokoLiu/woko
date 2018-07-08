4f4f6dda47320aa990303d3ec32872695d7fe571
刘玉龙 <ylliu@hillinsight.com>
2018-07-08 22:58:08 +0800

    feat(wtf): decimal number

---
+++ wtf/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/8 22:54
+# @Author  : Woko
+# @File    : __init__.py.py
---
+++ wtf/decimal_number.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/8 22:54
+# @Author  : Woko
+# @File    : decimal_number.py
+
+"""
+as you can see, 4.2 + 2.1 != 6.3
+so do not calculate float number directly
+use decimal
+"""
+
+from decimal import Decimal
+
+a = 4.2
+b = 2.1
+print a+b
+assert a+b != 6.3
+assert a+b != Decimal('6.3')
+
+aa = Decimal('4.2')
+bb = Decimal('2.1')
+print aa+bb
+assert aa+bb != 6.3
+assert aa+bb == Decimal('6.3')
