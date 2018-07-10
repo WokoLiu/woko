503d072c1667149ab5044f1d15afcdb1297378f4
刘玉龙 <ylliu@hillinsight.com>
2018-07-10 15:29:23 +0800

    feat(tools): wechat_info: get friends in a chatroom 获取一个微信群里自己的好友

--- tools/wechat_info.py
+++ tools/wechat_info.py
@@ -5,18 +5,20 @@
 
 
 import itchat
+from collections import defaultdict
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
 def login(hotReload=True):
-    itchat.auto_login(hotReload=hotReload)  # 实现登录状态保留，使你不用每运行一次就要扫一次二维码,默认是FALSE
+    itchat.auto_login(hotReload=hotReload)  # 实现登录状态保留，使得不用每运行一次就要扫一次二维码,默认是FALSE
     itchat.dump_login_status()
     print '登陆成功'
 
 
 def get_all_user_sex():
+    """计算所有好友的性别比"""
     friends = itchat.get_friends(update=True)[:]
     total = len(friends) - 1
     man = women = other = 0
@@ -35,16 +37,24 @@ def get_all_user_sex():
     print "其他：%.2f%%, %d人" % ((float(other) / total * 100), other)
 
 
-def get_chatroom_sex(chatroom_name):
+def search_first_chatroom(chatroom_name):
+    """根据群名称搜索群聊，并返回第一个"""
     room = itchat.search_chatrooms(chatroom_name)
     if not room:
-        return '没有找到该群'
+        return None
     if len(room) > 1:
         print '找到了%d个符合条件的群，取第一个了哈' % len(room)
     room = room[0]
     itchat.update_chatroom(userName=room['UserName'], detailedMember=True)
-    room = itchat.search_chatrooms(chatroom_name)
-    room = room[0]
+    return itchat.search_chatrooms(userName=room['UserName'])
+
+
+def get_chatroom_sex(chatroom_name):
+    """计算某个群里的性别比"""
+    room = search_first_chatroom(chatroom_name)
+    if not room:
+        print '没有找到该群'
+        return None
     members = room['MemberList']
     # print members
     man = women = other = 0
@@ -65,11 +75,34 @@ def get_chatroom_sex(chatroom_name):
     print "其他：%d人, %.2f%%" % (other, (float(other) / total * 100))
 
 
-def get_contacts():
-    contacts = itchat.get_contact()
-    return contacts
+def get_chatroom_friends(chatroom_name):
+    """获得某个群里自己的好友"""
+    room = search_first_chatroom(chatroom_name)
+    if not room:
+        print '没有找到该群'
+        return None
+    members = room['MemberList']
+    friends = get_friends()
+    result = []
+    for user in members:
+        if user.UserName in friends:
+            result.append(user)
+    print '群名称：', room.NickName
+    print '这个群里一共 %d 人是好友' % len(result)
+    for one in result:
+        print one.RemarkName, one.NickName
+    return result
+
+
+def get_friends():
+    """获取自己全部好友"""
+    contacts = itchat.get_friends(update=True)
+    friends = defaultdict(str)
+    for user in contacts:
+        friends[user.UserName] = user.NickName
+    return friends
 
 
 if __name__ == '__main__':
     login()
-    get_chatroom_sex('满天星')
+    get_chatroom_friends('游泳群')
