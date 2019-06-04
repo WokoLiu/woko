7ee0e85c10f9fa4e5c77d1ff0c9123736f5a6abb
刘玉龙 <ylliu@hillinsight.com>
2019-06-04 16:59:13 +0800

    feat(tools): add pushbear to server_chan

--- tools/server_chan/__init__.py
+++ tools/server_chan/__init__.py
@@ -12,7 +12,22 @@ import requests
 
 
 def wechat(text, desp):
-    with open('server.key', 'r') as f:
+    """send to wechat for one person"""
+    with open('./tools/server_chan/server.key', 'r') as f:
         secret = f.read()
     secret = secret.replace('\n', '')
-    requests.get('https://sc.ftqq.com/' + secret + '.send', params={'text': text, 'desp': desp})
+    requests.get('https://sc.ftqq.com/' + secret + '.send',
+                 params={'text': text, 'desp': desp})
+
+
+def wechat_channel(text, desp):
+    """send to wechat for all person subscribed thic channel"""
+    with open('./tools/server_chan/sendkey.key', 'r') as f:
+        send_key = f.read()
+    send_key = send_key.replace('\n', '')
+    params = {
+        'SendKey': send_key,
+        'text': text,
+        'desp': desp,
+    }
+    requests.get('https://pushbear.ftqq.com/sub', params=params)
